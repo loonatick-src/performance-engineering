3f766a121c797df142a3a6071d3f4be378263e6f
Chaitanya Kumar <chaitanyak28771@gmail.com>
2022-04-11 10:07:31 +0530

    Improve formating

--- Assignment1/matmul.c
+++ Assignment1/matmul.c
@@ -3,7 +3,6 @@
 #include <sys/time.h>
 #include "mmio.h"
 
-
 // default working set should not fit in L3 cache
 #define N  1024
 #define M  1024
@@ -12,24 +11,19 @@
 #define REP 10
 
 void matrix_mult(int m, int n, int p, float *A, float *B, float *C) {
-   int i, j, k;
-
-   for(i=0; i<m; i++) {
-      for(j=0; j<p; j++) {
-	C[i*p+j]=0;
-        for(k=0; k<n; k++) {
-                C[i*p+j] += A[i*n+k]*B[k*p+j];
-            }
-      }
+   for(int i=0; i<m; i++) {
+       for(int j=0; j<p; j++) {
+           C[i*p+j]=0;
+           for(int k=0; k<n; k++) {
+               C[i*p+j] += A[i*n+k]*B[k*p+j];
+           }
+       }
    }
 }
 
 void generate_mat(int m, int n, int p, float *A, float *B) {
-  int i;
-
-  for (i=0; i<(m*n); i++) A[i] = 1; //i/10; 
-  for (i=0; i<(n*p); i++) B[i] = 1; //i/5;
-
+    for (int i=0; i<(m*n); i++) A[i] = 1; //i/10; 
+    for (int i=0; i<(n*p); i++) B[i] = 1; //i/5;
 }
 
 void read_sparse(FILE *f, int m, int n, int nz, float *A) {
@@ -134,37 +128,37 @@ int read_mat(int *m, int *n, int *p, int *nzA, int *nzB, FILE* fa, FILE *fb) {
 }
 
 int main (int argc, char** argv) {
- float *A, *B, *C;
+    float *A, *B, *C;
 #ifdef TIMING
- struct timeval before, after;
+    struct timeval before, after;
 #endif
- int m, n, p, r, err;
- int nzA=0, nzB=0;
- FILE *fa, *fb, *fc; 
+    int m, n, p, r, err;
+    int nzA=0, nzB=0;
+    FILE *fa, *fb, *fc; 
  
 #ifdef GENERATE 
- m=M; n=N; p=P;
+    m=M; n=N; p=P;
 #else 
- if (argc < 3) {
-    fprintf(stderr, "Usage: %s [martix1] [matrix2] [resultmatrix] \n", argv[0]);
-    exit(1);
- }
- else {
-    if ((fa = fopen(argv[1], "rt")) == NULL) exit(1);
-    if ((fb = fopen(argv[2], "rt")) == NULL) exit(2);
-    err = read_mat(&m, &n, &p, &nzA, &nzB, fa,fb);    
-    if (err == -15) {
-	printf("Matrices are incompatible! \n");
-	fclose(fa); fclose(fb); 
-	exit(1);
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s [martix1] [matrix2] [resultmatrix] \n", argv[0]);
+        exit(1);
     }
- }
+    else {
+        if ((fa = fopen(argv[1], "rt")) == NULL) exit(1);
+        if ((fb = fopen(argv[2], "rt")) == NULL) exit(2);
+        err = read_mat(&m, &n, &p, &nzA, &nzB, fa,fb);    
+        if (err == -15) {
+    	    printf("Matrices are incompatible! \n");
+	        fclose(fa); fclose(fb); 
+	        exit(1);
+        }
+    }
 #endif
 
- A = (float *)calloc(m*n,sizeof(float));
- if (A==NULL) {printf("Out of memory A! \n"); exit(1);}
- B = (float *)calloc(n*p,sizeof(float));
- if (B==NULL) {printf("Out of memory B! \n"); exit(1);}
+    A = (float *)calloc(m*n,sizeof(float));
+    if (A==NULL) {printf("Out of memory A! \n"); exit(1);}
+    B = (float *)calloc(n*p,sizeof(float));
+    if (B==NULL) {printf("Out of memory B! \n"); exit(1);}
 
 #ifdef GENERATE
    generate_mat(m,n,p,A,B);
@@ -181,18 +175,18 @@ int main (int argc, char** argv) {
    fclose(fb);
 #endif
 
- C = (float *)calloc(m*p,sizeof(float));
- if (C==NULL) {printf("Out of memory C1! \n"); exit(1);}
-// C2 = (float *)calloc(N*P,sizeof(float));
-// if (C2==NULL) {printf("Out of memory C2! \n"); exit(1);}
+   C = (float *)calloc(m*p,sizeof(float));
+   if (C==NULL) {printf("Out of memory C1! \n"); exit(1);}
+   // C2 = (float *)calloc(N*P,sizeof(float));
+   // if (C2==NULL) {printf("Out of memory C2! \n"); exit(1);}
 
 //naive implementation 
 #ifdef TIMING
-  gettimeofday(&before, NULL); 
+   gettimeofday(&before, NULL); 
 #endif
-
-for (r=0; r<REP; r++) 
- matrix_mult(m,n,p,A,B,C);
+  for (r=0; r<REP; r++) {
+      matrix_mult(m,n,p,A,B,C);
+  }
 
 #ifdef TIMING
   gettimeofday(&after, NULL);
@@ -202,17 +196,14 @@ for (r=0; r<REP; r++)
 #endif
 
 #ifdef GENERATE
- if ((fc = fopen("gen_result.mtx", "wt")) == NULL) exit(3); 
+  if ((fc = fopen("gen_result.mtx", "wt")) == NULL) exit(3); 
 #else 
- if ((fc = fopen(argv[3], "wt")) == NULL) exit(3); 
+  if ((fc = fopen(argv[3], "wt")) == NULL) exit(3); 
 #endif   
- write_sparse(fc,m,p,C);
- fclose(fc);  
-
- free(A);
- free(B);
- free(C);
-// free(C2);
-
+  write_sparse(fc,m,p,C);
+  fclose(fc);  
+  free(A);
+  free(B);
+  free(C);
+  // free(C2);
 }
-
