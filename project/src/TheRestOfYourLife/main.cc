61f22651a602ad40d8015d7a7e4e2e3178b94264
Chaitanya Kumar <chaitanyak28771@gmail.com>
2022-04-27 17:15:43 +0530

    OpenMP pragmas, boost header
    Using my system installed `boost::multi_array` to store intermediate pixel values, `std::vector<std::vector<color>>` would have higher memory overhead, bare C-arrays would have error-prone book-keeping

--- project/src/TheRestOfYourLife/main.cc
+++ project/src/TheRestOfYourLife/main.cc
@@ -21,6 +21,7 @@
 
 #include <iostream>
 #include <omp.h>
+#include <boost/multi_array.hpp>
 
 color ray_color(
     const ray& r,
@@ -92,12 +93,14 @@ hittable_list cornell_box() {
 
 int main() {
     // Image
-
+    typedef boost::multi_array<double, 3> image_t;
     const auto aspect_ratio = 1.0 / 1.0;
     const int image_width = 600;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
     const int max_depth = 50;
+
+    image_t output_image(boost::extents[image_height][image_width][3]);
 
     // World
 
@@ -128,22 +131,30 @@ int main() {
     # pragma omp parallel
     {
         unsigned int thread_id = (unsigned int) omp_get_thread_num();
-        # pragma omp for
-        {
-            for (int j = image_height-1; j >= 0; --j) {
-                if (thread_id == 0)
-                    std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-                for (int i = 0; i < image_width; ++i) {
-                    color pixel_color(0,0,0);
-                    for (int s = 0; s < samples_per_pixel; ++s) {
-                        auto u = (i + random_double_r(&thread_id)) / (image_width-1);
-                        auto v = (j + random_double_r(&thread_id)) / (image_height-1);
-                        ray r = cam.get_ray(u, v);
-                        pixel_color += ray_color(r, background, world, lights, max_depth);
-                    }
-                    write_color(std::cout, pixel_color, samples_per_pixel);
+        # pragma omp for  // TODO: add clauses
+        
+        for (int j = image_height-1; j >= 0; --j) {
+            if (thread_id == 0)
+                std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+            for (int i = 0; i < image_width; ++i) {
+                color pixel_color(0,0,0);
+                for (int s = 0; s < samples_per_pixel; ++s) {
+                    auto u = (i + random_double_r(&thread_id)) / (image_width-1);
+                    auto v = (j + random_double_r(&thread_id)) / (image_height-1);
+                    ray r = cam.get_ray(u, v);
+                    pixel_color += ray_color(r, background, world, lights, max_depth);
                 }
+                // write_color(std::cout, pixel_color, samples_per_pixel);
+                output_image[i][j][0] = pixel_color.x();
+                output_image[i][j][1] = pixel_color.y();
+                output_image[i][j][2] = pixel_color.z();
             }
+        }
+    }
+
+    for (int i = image_height-1; i >= 0; i++) {
+        for (int j = 0; j < image_width; j++) {
+            write_color(std::cout, color(output_image[i][j][0], output_image[i][j][1], output_image[i][j][2]), samples_per_pixel);
         }
     }
     std::cerr << "\nDone.\n";
