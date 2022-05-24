5f66b6d84fa518ba149f96df801362c4fd125974
Chaitanya Kumar <chaitanyak28771@gmail.com>
2022-05-24 16:23:28 +0200

    Tiny benchmarks for random in sphere methods

---
+++ project/bench/rand_sphere_bench.cpp
@@ -0,0 +1,37 @@
+#include "rtweekend.h"
+#include "vec3.h"
+#include "omp.h"
+
+#include <iostream>
+#include <benchmark/benchmark.h>
+
+thread_local unsigned int seed;
+
+// thank you Noogle Benchmark and Chandler Caruth, very cool
+static void escape(void *p) {
+  asm volatile("" : : "g"(p) : "memory");
+}
+
+static void clobber() {
+  asm volatile("" : : : "memory");
+}
+
+static void BM_RandomSphereBaseline(benchmark::State& state) {
+    for (auto _: state) {
+        auto rand_vec = random_in_unit_sphere();
+        escape(&rand_vec);
+    }
+}
+
+static void BM_RandomSpherePolar(benchmark::State& state) {
+    for (auto _: state) {
+        // TODO: prevent compiler from optimising this out
+        auto rand_vec = random_in_unit_sphere_v2();
+        escape(&rand_vec);
+    }
+}
+
+BENCHMARK(BM_RandomSphereBaseline);
+BENCHMARK(BM_RandomSpherePolar);
+
+BENCHMARK_MAIN();
