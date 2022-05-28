289118b4e22c4f7d75355524d60d725da6b7d14b
Chaitanya Kumar <chaitanyak28771@gmail.com>
2022-05-28 12:51:06 +0200

    No longer using the modded baseline

--- project/bench/rand_sphere_bench.cpp
+++ project/bench/rand_sphere_bench.cpp
@@ -1,13 +1,13 @@
 #include "rtweekend.h"
 #include "vec3.h"
-#include "omp.h"
 
+#include <omp.h>
 #include <iostream>
 #include <benchmark/benchmark.h>
 
 thread_local unsigned int seed;
 
-// thank you Noogle Benchmark and Chandler Caruth, very cool
+// thank you Chandler Caruth, very cool
 static void escape(void *p) {
   asm volatile("" : : "g"(p) : "memory");
 }
@@ -38,16 +38,8 @@ static void BM_RandomSphereCartesian(benchmark::State& state) {
     }
 }
 
-static void BM_RandomSphereModdedbaseline(benchmark::State& state) {
-    for (auto _: state) {
-        auto rand_vec = random_in_unit_sphere_v4();
-        escape(&rand_vec);
-    }
-}
-
 BENCHMARK(BM_RandomSphereBaseline);
 BENCHMARK(BM_RandomSpherePolar);
 BENCHMARK(BM_RandomSphereCartesian);
-BENCHMARK(BM_RandomSphereModdedbaseline);
 
 BENCHMARK_MAIN();
