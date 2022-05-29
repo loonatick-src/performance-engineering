b7ae3eea2f3a4533c17ad1de5aabddff2fe1d694
kevin kuurman <k.kuurman@quicknet.nl>
2022-05-29 20:54:26 +0200

    file parser and such

--- project/graphs/timings_to_chart.py
+++ project/graphs/timings_to_chart.py
@@ -3,27 +3,8 @@ import numpy as np
 
 def originalScaling():
   sizes = [128, 256, 512, 1024, 2048]
-  sequential = [4.0751, 8.02996, 15.0113, 29.9553, 58.4266]
-
+  sequential = [4.06075, 8.03402, 14.9976, 29.8789, 57.3822]
   plt.plot(sizes, sequential, label="Sequential")
-  plt.xticks(sizes, sizes)
-  plt.ylabel('Time in seconds')
-  plt.xlabel('Output image height')
-  plt.legend()
-  plt.show()
-
-
-def beforeRandAndShared():
-  sizes = [128, 256, 512, 1024, 2048]
-  sequential = [4.39333, 10.1078, 16.1195, 32.6979, 61.5664]
-  omp_4 = [5.40449, 10.1135, 19.3992, 27.7449, 52.7681]
-  omp_8 = [6.00997, 11.7645, 19.8323, 30.4133, 57.8063]
-  omp_16 = [6.26695, 11.4344, 20.9347, 42.2966, 79.7227]
-
-  plt.plot(sizes, sequential, label="Sequential")
-  plt.plot(sizes, omp_4, label="Omp 4")
-  plt.plot(sizes, omp_8, label="Omp 8")
-  plt.plot(sizes, omp_16, label="Omp 16")
   plt.xticks(sizes, sizes)
   plt.ylabel('Time in seconds')
   plt.xlabel('Output image height')
@@ -81,6 +62,62 @@ def boxIntersectionBars():
   plt.show()
   
   
+def parseResultFile(filename):
+  content = open(filename, "r").read()
+  entries = [x for x in [[l for l in x.split("\n") if l != ''] for x in content.split("CONFIG")] if len(x) > 1]
+  parsed = [
+    {
+      'threads': int(entry[0].split(" = ")[1]),
+      'width': int(entry[1].split(" = ")[1]),
+      'height': int(entry[2].split(" = ")[1]),
+      'max_depth': int(entry[3].split(" = ")[1]),
+      'samples_per_pixel': int(entry[4].split(" = ")[1]),
+      'seconds_to_render': float(entry[5].split(" ")[-2])
+    }
+    for entry in entries
+  ]
+  for p in parsed:
+    p['total_rays'] = p['width'] * p['height'] * p['samples_per_pixel']
+    p['seconds_per_ray'] = p['seconds_to_render'] / p['total_rays']
+    p['rays_per_second'] = 1.0 / p['seconds_per_ray']
+
+  return parsed
+
+def original_versions():
+  parsed = parseResultFile("../measurements/v0_results.txt")
   
+  sizes =  [128, 256, 512, 1024, 2048]
+  sequential =  [next(e for e in parsed if e['threads'] == 1 and e['height'] == s)['seconds_to_render'] for s in sizes]
+
+  plt.plot(sizes, sequential, label="Base framework")
+  plt.xticks(sizes, sizes)
+  plt.ylabel('Time in seconds')
+  plt.xlabel('Output image height')
+  plt.legend()
+  plt.show()
+
+def parallel_versions(filename):
+  base = parseResultFile("../measurements/v0_results.txt")
+  parsed = parseResultFile(filename)
   
-boxIntersectionBars()
+  sizes =  [128, 256, 512, 1024, 2048]
+  sequential =  [next(e for e in base if e['threads'] == 1 and e['height'] == s)['seconds_to_render'] for s in sizes]
+  omp_1 =  [next(e for e in parsed if e['threads'] == 1 and e['height'] == s)['seconds_to_render'] for s in sizes]
+  omp_2 =  [next(e for e in parsed if e['threads'] == 2 and e['height'] == s)['seconds_to_render'] for s in sizes]
+  omp_4 =  [next(e for e in parsed if e['threads'] == 4 and e['height'] == s)['seconds_to_render'] for s in sizes]
+  omp_8 =  [next(e for e in parsed if e['threads'] == 8 and e['height'] == s)['seconds_to_render'] for s in sizes]
+  omp_16 = [next(e for e in parsed if e['threads'] == 16 and e['height'] == s)['seconds_to_render'] for s in sizes]
+
+  plt.plot(sizes, sequential, label="Base framework")
+  plt.plot(sizes, omp_1, label="Omp 1")
+  plt.plot(sizes, omp_2, label="Omp 2")
+  plt.plot(sizes, omp_4, label="Omp 4")
+  plt.plot(sizes, omp_8, label="Omp 8")
+  plt.plot(sizes, omp_16, label="Omp 16")
+  plt.xticks(sizes, sizes)
+  plt.ylabel('Time in seconds')
+  plt.xlabel('Output image height')
+  plt.legend()
+  plt.show()
+
+parallel_versions("../measurements/v6_results.txt")
